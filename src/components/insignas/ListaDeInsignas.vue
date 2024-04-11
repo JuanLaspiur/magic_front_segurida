8e8eb0f44c792f457ef1743d6a9af6516f87f182
JuanLaspiur <laspiurperezjuan@gmail.com>
2024-04-11 15:30:31 -0300

    qwwerty

--- src/components/insignas/ListaDeInsignas.vue
+++ src/components/insignas/ListaDeInsignas.vue
@@ -2,16 +2,16 @@
   <div>
     <h5>Lista de Insignias</h5>
     <q-list>
-      <q-item v-for="insignia in insignias" :key="insignia.id">
+      <q-item v-for="insignia in insignasDeLaApi" :key="insignia.id">
         <q-avatar style="margin-right: 10px">
           <img :src="insignia.imagen" alt="Imagen de Insignia" />
         </q-avatar>
         <div class="q-item-main">
-          <p>{{ insignia.nombre }}</p>
-          <p>{{ insignia.descripcion }}</p>
+          <p>{{ insignia.name }}</p>
+          <p>{{ insignia.description }}</p>
         </div>
-        <div class="q-item-side text-center" style="padding: 22px;">
-          <q-btn color="red" icon="delete" @click="removeInsignia(insignia)" />
+        <div class="q-item-side text-center" style="padding: 22px">
+          <q-btn color="red" icon="delete" @click="confirmDelete(insignia)" />
         </div>
       </q-item>
     </q-list>
@@ -22,38 +22,37 @@
 export default {
   data () {
     return {
-      insignias: []
+      insignasDeLaApi: [] // Inicializar insignasDeLaApi como un arreglo vacío
+    }
+  },
+  methods: {
+    confirmDelete (insignia) {
+      if (confirm('¿Estás seguro de que deseas eliminar esta insignia?')) {
+        this.$api
+          .delete(`/insignas/${insignia._id}`)
+          .then(() => {
+            // Eliminar la insignia de la lista local
+            this.insignasDeLaApi = this.insignasDeLaApi.filter(
+              item => item._id !== insignia._id
+            )
+          })
+          .catch(error => {
+            console.error('Error al eliminar la insignia:', error)
+          })
+      }
+    },
+    async fetchTraerInsignas () {
+      try {
+        const response = await this.$api.get('/insignas')
+        this.insignasDeLaApi = response
+        console.log('Llamado a la api ' + this.insignasDeLaApi)
+      } catch (error) {
+        console.error('Error fetching badges:', error)
+      }
     }
   },
   mounted () {
-    // Implementa la lógica para obtener la lista de insignias (por ejemplo, desde una API)
-    this.insignias = [
-      {
-        id: 1,
-        nombre: 'Insignia de Bronce',
-        descripcion: 'Primera insignia otorgada por completar X logros',
-        imagen: 'https://example.com/imagen-insignia-bronce.png'
-      },
-      {
-        id: 2,
-        nombre: 'Insignia de Plata',
-        descripcion: 'Segunda insignia otorgada por completar Y logros',
-        imagen: 'https://example.com/imagen-insignia-plata.png'
-      },
-      {
-        id: 3,
-        nombre: 'Insignia de Oro',
-        descripcion: 'Tercera insignia otorgada por completar Z logros',
-        imagen: 'https://example.com/imagen-insignia-oro.png'
-      }
-    ]
-  },
-  methods: {
-    removeInsignia (insignia) {
-      // Implementa la lógica para eliminar la insignia (por ejemplo, a través de una API)
-      console.log('Insignia eliminada:', insignia)
-      this.insignias = this.insignias.filter(i => i.id !== insignia.id)
-    }
+    this.fetchTraerInsignas() // Llamar a fetchTraerInsignas cuando el componente se monta
   }
 }
 </script>
--- src/pages/admin/ModuloInsignas.vue
+++ src/pages/admin/ModuloInsignas.vue
@@ -3,15 +3,17 @@
     <h4>Gestión de insignas</h4>
     <div style="display: flex; gap: 20%">
       <CrearInsigna style="max-width: 500px" />
-      <ListaDeInsignas style="max-width: 500px" />
+     <ListaDeInsignas  style="max-width: 500px" />
     </div>
-    <div style="margin-top: 6%;">
-      <q-btn color="primary" label="Asignar Insignias a Usuarios" @click="$router.push('/modulo_insignas-usuarios')" />
-
+    <div style="margin-top: 6%">
+      <q-btn
+        color="primary"
+        label="Asignar Insignias a Usuarios"
+        @click="$router.push('/modulo_insignas-usuarios')"
+      />
     </div>
   </div>
 </template>
-
 <script>
 import CrearInsigna from '../../components/insignas/CrearInsigna'
 import ListaDeInsignas from '../../components/insignas/ListaDeInsignas'
