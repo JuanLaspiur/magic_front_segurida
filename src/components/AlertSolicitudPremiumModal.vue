439dc6bb74d482ff54576b56d5d319aeacd067e6
JuanLaspiur <laspiurperezjuan@gmail.com>
2024-05-30 13:04:11 -0300

    Modulo para solicitud con buena estetica

--- src/components/AlertSolicitudPremiumModal.vue
+++ src/components/AlertSolicitudPremiumModal.vue
@@ -1,9 +1,10 @@
 <template>
   <div v-if="visible" class="modal-overlay" @click="close">
     <div class="modal-content" @click.stop>
-      <h3>{{ title }}</h3>
-      <p>{{ message }}</p>
-      <button @click="close">Cerrar</button>
+      <img src="../assets/logo_final_png-02.png" alt="Logo" class="modal-logo">
+      <h3 class="modal-title">{{ title }}</h3>
+      <p class="modal-message">{{ message }}</p>
+      <button class="close-button" @click="close">Cerrar</button>
     </div>
   </div>
 </template>
@@ -43,13 +44,49 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index:999999;
 }
+
 .modal-content {
-  background: rgb(216, 47, 47);
+  background: #f0f0f0;
   padding: 20px;
   border-radius: 8px;
   max-width: 400px;
   width: 100%;
   text-align: center;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.modal-logo {
+  width: 150px;
+  margin-bottom: 20px;
+}
+
+.modal-title {
+  margin-top: 0;
+  margin-bottom: 10px;
+  color: #333;
+  font-size: 24px;
+}
+
+.modal-message {
+  margin-bottom: 20px;
+  color: #666;
+}
+
+.close-button {
+  background-color: #f44336;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+  border-radius: 4px;
+  transition: background-color 0.3s ease;
+}
+
+.close-button:hover {
+  background-color: #d32f2f;
 }
 </style>
